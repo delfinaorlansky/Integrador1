937c7ea858b24f1f32ecbe9297e55de8e488ed39
mjaichenco <56735038+mjaichenco@users.noreply.github.com>
2021-04-21 17:22:03 -0300

    sassas

--- tarjetas-app/src/pages/Home.js
+++ tarjetas-app/src/pages/Home.js
@@ -1,24 +1,53 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect} from 'react';
 import {Container,Row,Col,Card, Button,Accordion} from 'react-bootstrap';
 import {format} from 'date-fns';
 
 export default function Home() {
     const [cards, setCards] = useState([]); 
+    const [quantity,setQuantity] = useState(9);
+    const viewMore = () => { setQuantity(quantity +3); filldata(); };
+    const filldata = () => {  fetch('https://randomuser.me/api/?results= ' +quantity)
+    .then(response => response.json())
+    .then(data => setCards(data.results)); };
     useEffect(() => {
-        fetch('https://randomuser.me/api/?results=10')
-  .then(response => response.json())
-  .then(data => setCards(data.results));
-      },[]);
+    
+    filldata();
+       
+    }
+      ,[]);
+      const removeCard = uuid => {
+
+        const cardsRemoved = cards.filter(card => card.login.uuid !== 
+    uuid);
+         setCards(cardsRemoved);
+      };
+    
+
+
+
     return (
+
      <Container>
+          {/* <Row><Col><Form>
+  <Form.Group controlId="formBasicEmail">
+    <Form.Label>Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+  </Form>
+  </Col></Row> */}
        <Row>
        {cards.map((card) => (
               <Col className="p-3">
-                <Accordion defaultActiveKey="0">
+                <Accordion defaultActiveKey="1">
                 <Card style={{ width: '18rem'}}>
                 <Card.Img variant="top" src={card.picture.large}/>
                 <Card.Body>
-                  <Card.Title>{card.name.first} {card.name.last} </Card.Title>
+                  <Card.Title>{card.name.first} {card.name.last} <Button  onClick={() =>
+removeCard(card.login.uuid)}>X</Button>
+ </Card.Title>
                   <Card.Text> 
                   <a href={ "mailto:"+ card.email}>{card.email} </a>
                   <br></br> 
@@ -43,11 +72,12 @@ export default function Home() {
               </Col>
 ))}   
      </Row>
+
+     <Row> <Col><Button type="button" onClick={viewMore} >Ver mas</Button></Col></Row>
+    
+
+
     </Container>
 
-    
-    
-    );
-    
-  }
 
+      );}
